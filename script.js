572c076967939116d6f77ee29be1176d463bd7bf
BabyDecent <ukohajoseph99@gmail.com>
2023-06-16 05:12:38 -0700

    Welcome to JavaScript!

---
+++ script.js
@@ -0,0 +1,14 @@
+// values and variables
+console.log("Hello World");
+
+
+let firstName= "Donald";
+
+console.log(firstName);
+
+
+//Varible Naming Cnventions
+let last_name2 ="John"
+let $function ="50"
+
+console.log(last_name2);
