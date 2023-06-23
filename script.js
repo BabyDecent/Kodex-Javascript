76213749db3a0046bb1546b7130742c3409e118f
BabyDecent <ukohajoseph99@gmail.com>
2023-06-23 03:27:41 -0700

    coding challange correction

--- script.js
+++ script.js
@@ -12,3 +12,86 @@ let last_name2 ="John"
 let $function ="50"
 
 console.log(last_name2);
+
+// ***************Basic Operators**********/
+
+// Math Operators
+const now =2023;
+const divineAge = now -1990;
+const davidAge = now - 1981;
+
+console.log(divineAge, davidAge);
+console.log(divineAge * 2, divineAge /10, 2 ** 3);
+
+/** 
+* Create 2 variables
+* The datatype must be strings
+* Concatenate (Add) the two variables
+* And display the result in the console
+*/
+
+
+
+
+// ------ASSIGNMENT OPERATOR (=)-----
+let numberOne =10 + 15;
+numberOne +5;
+console.log(numberOne);
+// numberOne = numberOne + 5;  
+
+let numberTwo = 2 + 5;
+numberTwo *= 7;
+numberTwo = numberTwo * 7;
+console.log(numberTwo);
+
+
+let numberThree = 1;
+numberThree++;
+// can be rewritten as:
+numberThree  = numberThree + 1;
+console.log(numberThree);
+
+let numberFour = 4;
+numberFour--;
+// can be rewritten as:
+numberFour = numberFour - 1;
+console.log(numberFour);
+
+// ----Comparison Operators ( >, <, >=, <=)-----------
+let ugoAge, olisaAge;
+ugoAge = 32;
+olisaAge = 35;
+console.log(ugoAge === olisaAge);
+
+// -----Operetor Precedence--------------------
+const yearNow = 2023;
+const josephAge = yearNow - 1999;
+const henryAge = yearNow - 1990;
+console.log((josephAge + henryAge) * 10);
+// 22 + 33 * 10
+
+
+
+const markMass = 78;
+const markHeight = 1.69;
+
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2; 
+const johnkBMI = johnMass / johnHeight ** 2; 
+
+console.log(markBMI, johnkBMI);
+
+const markHeightBMI = markBMI > johnkBMI;
+console.log (markHeightBMI);
+const johnHeightBMI =  johnkBMI > markBMI;
+
+
+
+
+
+
+ 
+
+
