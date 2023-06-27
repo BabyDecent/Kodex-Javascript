de6eaaa076e1572c1e4f5be1e41148f73b929d45
BabyDecent <ukohajoseph99@gmail.com>
2023-06-27 07:29:15 -0700

    javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// values and variables
+// ***************** values and variables **************/
 console.log("Hello World");
 
 
@@ -7,13 +7,13 @@ let firstName= "Donald";
 console.log(firstName);
 
 
-//Varible Naming Cnventions
+// ***************** Varible Naming Cnventions ***********/
 let last_name2 ="John"
 let $function ="50"
 
 console.log(last_name2);
 
-// ***************Basic Operators**********/
+// *************** Basic Operators ************/
 
 // Math Operators
 const now =2023;
@@ -86,6 +86,118 @@ console.log(markBMI, johnkBMI);
 const markHeightBMI = markBMI > johnkBMI;
 console.log (markHeightBMI);
 const johnHeightBMI =  johnkBMI > markBMI;
+
+// *************** Strings and Template Literals ****************
+const myName = 'Donald';
+const myJob = "Developer";
+const yearofBirth = 1717;
+const currentYear = 2077;
+
+const donald = "I'm " + myName + ', a ' + (currentYear - yearofBirth) + ' year old ' + myJob + '!';
+console.log(donald);
+
+const newDonald = `I'm ${myName}, a ${currentYear - yearofBirth} year old ${myJob}!`;
+console.log(newDonald);
+
+console.log(`Hello how are you doing`);
+
+console.log(`This is \n\
+for multiple \n\
+lines`);
+
+console.log(`This
+is
+for multiple
+lines`)
+
+
+
+//******************* Taking Decision: if / else syayement ****************/
+const kelsAge = 5;
+
+// If statement only executes when the expression evaluated to true
+if(kelsAge >= 18){
+    console.log(`kels is eligible to drink Alcohol cos his age is ${kelsAge}`);    
+}
+
+// Eles statement executes when the if statement returns false
+else{
+    const yearsLeft = 18 - kelsAge;
+    console.log(`kels is not eligible to drink alcohol . Wait another ${yearsLeft} years`)
+}
+
+let dayofTheWeek = 'Monday';
+
+if(dayofTheWeek == 'Monday'){
+    console.log('SIT AT HOME')
+} 
+else{
+    console.log('Write some code!');
+}
+
+
+
+if(markBMI > johnkBMI){
+    console.log(`mark's BMI ${markBMI} is higher than John's ${johnkBMI}!`)
+} 
+
+else{
+    console.log(`John's BMI ${johnkBMI} is higher than Marks's ${markBMI}!`)
+} 
+
+// ************* Type Coversion and coercion ********
+// Type conversion is the processing of converting a value from one type to another
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 10);
+
+console.log(Number('Donald'));
+console.log(typeof NaN);
+
+// Type Coersion
+// Tyope Coersion is when Javascript coerces or changes a data type by itself
+const sum = 35 + 'hello';
+console.log(sum)
+
+console.log('23' - '10' - '3');
+
+console.log('24' / '2');
+
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+
+// We have 5 Falsey Values: 0, '', undefined, null, Nan 
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+
+const money = 100;
+
+if(money){
+    console.log('Go on a shopping spree!');    
+}
+else{
+    console.log('Dont spend the money');
+}
+
+let product = 0;
+if(product){
+      console.log('stay at home');      
+} 
+else{
+    console.log('Go on a shopping spree!');
+} 
+
+// ************** Logical Operators ***************
+
+
+
+
+
+
 
 
 
